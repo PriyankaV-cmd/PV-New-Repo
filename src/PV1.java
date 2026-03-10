8d62c28179da1601950c95ede995f45c770d179d
PRIYANKA <priyanka@PRIYANKAs-MacBook-Air.local>
2026-03-11 03:37:47 +0530

    Add PV1.java

---
+++ src/PV1.java
@@ -0,0 +1,8 @@
+public class PV1{
+    public static void main(String[] args) {
+        System.out.println("PV1 working!");
+        if (args.length > 0) {
+            System.out.println("First argument: " + args[0]);
+        }
+    }
+}
