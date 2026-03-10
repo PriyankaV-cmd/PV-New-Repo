2d7a9ce793a99d9854770f916f9a7a23b76d0e75
PRIYANKA <priyanka@PRIYANKAs-MacBook-Air.local>
2026-03-11 03:39:53 +0530

    Add PV2.java

---
+++ src/PV2.java
@@ -0,0 +1,8 @@
+public class PV2{
+    public static void main(String[] args) {
+        System.out.println("PV2 working!");
+        if (args.length > 0) {
+            System.out.println("First argument: " + args[0]);
+        }
+    }
+}
