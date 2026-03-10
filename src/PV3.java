64ed7bbf604fd0b5e395a1be5c29cce62fce779a
PRIYANKA <priyanka@PRIYANKAs-MacBook-Air.local>
2026-03-11 03:41:01 +0530

    Add PV3.java

---
+++ src/PV3.java
@@ -0,0 +1,8 @@
+public class PV3{
+    public static void main(String[] args) {
+        System.out.println("PV3 working!");
+        if (args.length > 0) {
+            System.out.println("First argument: " + args[0]);
+        }
+    }
+}
